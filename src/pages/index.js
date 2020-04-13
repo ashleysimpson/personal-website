87d68dc405689f78ede5fa1569b71f1d452032e3
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2020-04-13 10:53:34 -0400

    add prettier config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import Layout from "../components/layout";
+import Layout from '../components/layout';
 
 export default () => {
   const [repos, setRepos] = useState([]);
