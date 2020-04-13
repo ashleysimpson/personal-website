ffe59acd6b5736e541a083f501bd73292ca54bd6
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2020-04-13 15:06:57 -0400

    add about me

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Layout from '../components/layout';
 import DiagonalAccent from '../components/diagonalAccent';
 import Name from '../components/name';
+import AboutMe from '../components/aboutMe';
 
 export default () => {
   return (
     <Layout>
       <DiagonalAccent />
       <Name />
-      <div className="box aboutMe"></div>
+      <AboutMe />
       <div className="box image"></div>
       <div className="box reachOut"></div>
     </Layout>
