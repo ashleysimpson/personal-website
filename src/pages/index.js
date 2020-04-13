a89072e27dd3905977aea821a2d5293ef9897968
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2020-04-13 11:35:03 -0400

    add basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,11 @@ import Layout from '../components/layout';
 export default () => {
   return (
     <Layout>
-      <h1>Ashley Simpson</h1>
-      <p>Here is some information about me!</p>
+      <div className="box diagonal"></div>
+      <div className="box name"></div>
+      <div className="box aboutMe"></div>
+      <div className="box image"></div>
+      <div className="box reachOut"></div>
     </Layout>
   );
 };
