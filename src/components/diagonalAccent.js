eacb70668e708463dc853ad2066a45f0b03a9ebd
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2020-04-13 16:26:33 -0400

    add preliminary color scheme

--- src/components/diagonalAccent.js
+++ src/components/diagonalAccent.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function DiagonalAccent() {
   return (
     <div className="diagonal">
-      <svg viewBox="0 0 200 100">
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 200 100"
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <polygon points="0,0 0,100, 200,0" fill="currentColor" />
       </svg>
     </div>
