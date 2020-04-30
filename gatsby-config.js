fbfb06c5bbe066eb42814ee7a29abda162a4f375
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2020-04-30 08:30:47 -0400

    add plugin for icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,18 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ashley Simpson`,
+        short_name: `Ashley Simpson`,
+        start_url: `/`,
+        background_color: `#1b2a41`,
+        theme_color: `#e6b542`,
+        display: `standalone`,
+        icon: `src/images/ashleysimpson.png`,
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
