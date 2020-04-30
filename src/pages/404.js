f2f3be324563389d9ffb897fa3ebb4354f74ba51
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2020-04-30 08:07:13 -0400

    add placeholder content to prevent flicker

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,19 @@
+import React from 'react';
+
 import { useEffect } from 'react';
 import { navigate } from 'gatsby';
+
+import Layout from '../components/layout';
+import DiagonalAccent from '../components/diagonalAccent';
 
 export default function NotFound() {
   useEffect(() => {
     navigate('/');
   }, []);
 
-  return null;
+  return (
+    <Layout>
+      <DiagonalAccent />
+    </Layout>
+  );
 }
