97b118030da63fd314e134a03eabbac6bb35ba17
Ashley Simpson <ashley.simpson@mail.mcgill.ca>
2026-01-05 11:08:12 -0800

    Update location from Montreal to Vancouver

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -4,7 +4,7 @@ export default function AboutMe() {
   return (
     <div className="aboutMe">
       <p>
-        Hi! I'm a full stack developer currently living in Montreal, Canada.
+        I'm a full stack developer currently living in Vancouver, Canada.
         Feel free to reach out on LinkedIn if you want to connect. Also, you can
         check out my GitHub profile to see any personal projects I am currently
         working on.
