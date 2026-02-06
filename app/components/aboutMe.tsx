09a9ca066ee588be9d9b7b53247f86c9d8b19e97
Ashley Simpson <ashley.simpson@mail.mcgill.ca>
2026-02-06 08:19:07 -0800

    adjust css (#21)

--- app/components/aboutMe.tsx
+++ app/components/aboutMe.tsx
@@ -1,6 +1,6 @@
 export default function AboutMe(): React.ReactElement {
   return (
-    <p className="font-opensans text-textNormal text-2xl mx-8 mt-8 tablet:text-4xl tablet:leading-snug tablet:m-14 tablet:[grid-area:3/1/4/3] desktop:text-5xl desktop:leading-snug desktop:m-14 desktop:[grid-area:3/1/4/3]">
+    <p className="font-opensans text-textNormal text-xl mx-8 mt-8 tablet:mt-24 tablet:text-2xl tablet:leading-snug tablet:m-14 tablet:[grid-area:3/1/4/3] desktop:text-3xl desktop:leading-snug desktop:m-14 desktop:[grid-area:3/1/4/3]">
       I&apos;m a full stack developer currently living in Vancouver Canada. Feel
       free to reach out on LinkedIn if you want to connect. You can also check
       out my GitHub profile to see any personal projects I am currently working
